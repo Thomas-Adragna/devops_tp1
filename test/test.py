a0b87a8d666876b48666e1c01d3a4fe143eff054
Hugo Leyx-valade <hleyx-valade@hellowork.com>
2026-04-15 11:33:16 +0200

    feat: sonarqube

--- test/test.py
+++ test/test.py
@@ -1,1 +1,7 @@
-print("Hi Triks, this is a test")
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
+
+def test_main_compiles():
+    import main
+    assert main is not None
